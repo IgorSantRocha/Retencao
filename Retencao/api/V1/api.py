fa451c0e8ba34046bf10bcaf6f81c96efb4963a1
Igor Santos Rocha <132116596+IgorSantRocha@users.noreply.github.com>
2024-02-14 09:20:55 -0300

    Delete Retencao directory

--- Retencao/api/V1/api.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-from api.V1.endpoints import abertura
-
-api_router = APIRouter()
-api_router.include_router(
-    abertura.router, prefix='/abertura', tags=["Abertura"])
--- Retencao/models/__all_models.py
+++
@@ -1,1 +0,0 @@
-from models.abertura_model import RetencaoAbModel, ProjetoFdModel
