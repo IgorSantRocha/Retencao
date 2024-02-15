d32e07bb602fe55d0dd1f0b2d9d64ce6092caa66
IgorSantRocha <igor77876@gmail.com>
2024-02-15 09:14:57 -0300

    Ajustado exemplo de config

--- core/exemplo-configs.py
+++ core/exemplo-configs.py
@@ -10,7 +10,6 @@ class Settings(BaseSettings):
     Congigurações gerais da API
     '''
     API_V1_STR: str = '/api/v1'
-    # DB_URL: str = "mssql+aioodbc://fastapi:Profeta#2@DB-03:1433/teste?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes"
     DB_URL: str = "mssql+aioodbc://USUARIO:SENHA@IP_OU_NOME_SERVIDOR:PORTA/NOME_DB?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes"
     DBBaseModel: DeclarativeMeta = declarative_base()
 
