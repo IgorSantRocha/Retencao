b9e721e0127287dc11fbdbf1edaabcc86769ccea
IgorSantRocha <igor77876@gmail.com>
2024-02-15 19:54:33 -0300

    Alterado para funcionar também a Redoc

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.docs import get_swagger_ui_html, get_redoc_html
 from fastapi.openapi.utils import get_openapi
 
 from core.configs import settings
@@ -9,30 +9,33 @@ app = FastAPI(title='Integração Central de Retenção',
               root_path="/Retencao",
               version='0.0.1',
               description='API criada para integração do chatbot com o sistema da Central de Retenção',
-              contact={
-                  "name": "C-Trends BPO!",
-                  "url": "https://www.c-trends.com.br/",
-                  "email": "ti@c-trends.com.br",
-              })
+              )
 app.include_router(api_router, prefix=settings.API_V1_STR)
-
-
-@app.get("/Retencao/docs", include_in_schema=False)
-async def custom_swagger_ui_html():
-    return get_swagger_ui_html(openapi_url="/Retencao/openapi.json", title="API Docs")
-
-# Rota para o esquema OpenAPI
-
-
-@app.get("/Retencao/openapi.json", include_in_schema=False)
-async def get_custom_openapi():
-    return get_openapi(title="API Docs", version="0.0.1", routes=app.routes)
 
 
 @app.get("/Retencao/", description='Resposta somente para validar se a API subiu corretamente. Sem nenhuma conexão com o banco de dados.',
          summary='Valida se API está no ar')
 def get_index():
     return {'msg': 'API está no ar!'}
+
+
+@app.get("/Retencao/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(openapi_url="/Retencao/openapi.json", title='API Docs')
+
+# Rota para a documentação Redoc
+
+
+@app.get("/Retencao/redoc", include_in_schema=False)
+async def redoc_html():
+    return get_redoc_html(openapi_url="/Retencao/openapi.json", title='ReDoc')
+
+'''Rota para o esquema OpenAPI'''
+
+
+@app.get("/Retencao/openapi.json", include_in_schema=False)
+async def get_custom_openapi():
+    return get_openapi(title=app.title, version="0.0.1", routes=app.routes, description=app.description)
 
 
 if __name__ == '__main__':
